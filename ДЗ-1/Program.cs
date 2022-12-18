44e34cdd654be3d324b52467a59039a6e0560d9d
Akbar Dzhamilov <akbarshoh-95@mail.ru>
2022-12-18 08:28:42 +0300

    решение задачи и добавление файла .gitignore

--- ДЗ-1/Program.cs
+++ ДЗ-1/Program.cs
@@ -4,4 +4,11 @@ a = 5; b = 7 -> max = 7
 a = 2 b = 10 -> max = 10
 a = -9 b = -3 -> max = -3*/
 
- 
+ Console.Write("Введите первое число: ");
+ double firstNumber = Convert.ToDouble(Console.ReadLine());
+ Console.Write("Введите второе число: ");
+ double secondNumber = Convert.ToDouble(Console.ReadLine());
+ double max = firstNumber;
+ if (max < secondNumber)
+    Console.WriteLine($"{secondNumber} больше чем {firstNumber}");
+ else Console.WriteLine($"{firstNumber} больше чем {secondNumber} ");
