e1f067ec3225861d256401c33493af4ea264df64
Akbar Dzhamilov <akbarshoh-95@mail.ru>
2022-12-22 19:29:30 +0300

    решение задачи 10

---
+++ ДЗ-2/задача_10/Program.cs
@@ -0,0 +1,25 @@
+﻿/*Задача 10: Напишите программу, которая принимает на вход трёхзначное число 
+и на выходе показывает вторую цифру этого числа.
+
+456 -> 5
+782 -> 8
+918 -> 1*/
+
+Console.Write("Введите целое трёхзначное число: ");
+string str = Console.ReadLine();
+if(str.Length == 3)
+{
+    int thirdNumber = int.Parse(str);
+    int secondNumber = thirdNumber % 100;
+    int firsNumber = secondNumber % 10;
+    int result = (secondNumber - firsNumber) / 10;
+    Console.WriteLine($"вторая цифра этого числа {thirdNumber} = {result}");
+}
+if(str.Length > 3)
+    Console.WriteLine("Вы ввели больше трёхзначного");
+if(str.Length < 3)
+    Console.WriteLine("Вы ввели меньше трёхзначного");
+else 
+    Console.WriteLine("Введите корректное число");
+
+
