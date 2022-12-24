4fbc621aeea4ebd9c8d8a77c38d3d5f8ca298c06
Akbar Dzhamilov <akbarshoh-95@mail.ru>
2022-12-24 08:57:30 +0300

    решение задачи 19

---
+++ ДЗ-3/задача_19/Program.cs
@@ -0,0 +1,17 @@
+﻿/*Напишите программу, которая принимает на вход пятизначное число 
+и проверяет, является ли оно палиндромом.
+
+14212 -> нет
+12821 -> да
+23432 -> да */
+
+Console.Write("Введите целое пятизначное число: ");
+string userNumber = Console.ReadLine();
+int Number = int.Parse(userNumber);
+if(userNumber.Length == 5)
+{
+    if(userNumber[userNumber.Length-1] == userNumber[0] & userNumber[userNumber.Length-2] == userNumber[1])
+    Console.WriteLine($"{Number} => да ");
+    else Console.WriteLine($"{Number} => нет ");
+}
+else Console.WriteLine("Введите корректное, целое пятизначное число");
