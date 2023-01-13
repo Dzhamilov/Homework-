f7b170e61c6aa136e1bd7757f1c5396be0d8f58e
Akbar Dzhamilov <akbarshoh-95@mail.ru>
2023-01-13 21:50:44 +0300

    решение задачи 27

---
+++ ДЗ-4/задача_27/Program.cs
@@ -0,0 +1,29 @@
+﻿/*Напишите программу, которая принимает на вход число
+ и выдаёт сумму цифр в числе.
+452 -> 11
+82 -> 10
+9012 -> 12 */
+
+int ReadNumber(string msg)
+{
+    Console.Write(msg);
+    return int.Parse(Console.ReadLine());
+}
+
+void SummNumber(int number)
+{
+    int n = number,
+        sum = 0,
+        digit = 0;
+    while(n > 0)
+    {
+        digit = n % 10;
+        sum += digit;
+        n /= 10;
+    }
+    Console.Write($"{number} -> {sum}");
+}
+
+int number = ReadNumber("Введите число: ");
+
+SummNumber(number);
