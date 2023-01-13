a1ef28eebb2102cec8b46edd8bfaf5b2dec123ae
Akbar Dzhamilov <akbarshoh-95@mail.ru>
2023-01-13 21:21:38 +0300

    решение задачи 25

---
+++ ДЗ-4/задача_25/Program.cs
@@ -0,0 +1,26 @@
+﻿/*Напишите цикл, который принимает на вход два числа 
+(A и B) и возводит число A в натуральную степень B.
+3, 5 -> 243 (3⁵)
+2, 4 -> 16
+Не использовать Math.Pow() и аналоги!*/
+
+int ReadNumber(string msg)
+{
+    Console.Write(msg);
+    return int.Parse(Console.ReadLine());
+}
+
+void PowNumber(int number, int degree)
+{
+    int result = 1;
+    for (int i = 0; i < degree; i++)
+    {
+        result *= number; 
+    }
+    Console.Write($"{number} ^ {degree} = {result}");
+}
+
+int number = ReadNumber("Введите число: ");
+int degree = ReadNumber("Введите показатель степени: ");
+
+PowNumber(number, degree);
