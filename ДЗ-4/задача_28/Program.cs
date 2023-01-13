e9fff9c9a3cc943c22cdbee2e7fe740105aea50c
Akbar Dzhamilov <akbarshoh-95@mail.ru>
2023-01-13 22:27:03 +0300

    решение задачи 28

---
+++ ДЗ-4/задача_28/Program.cs
@@ -0,0 +1,26 @@
+﻿/*Напишите программу, которая задаёт массив из 8 элементов и 
+выводит их на экран. Данные вводятся с консоли пользователем */
+
+int[] GreateArray(int size)
+{
+    int[] arr = new int[size];
+    for (int i = 0; i < size; i++)
+    {
+        Console.Write($"Введите {i}-й элемент массива: ");
+        arr[i] = int.Parse(Console.ReadLine());
+    }
+
+    return arr;
+}
+
+void PrintArray(int[] arr)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        Console.Write($"{arr[i]}, ");
+    }
+    Console.WriteLine();
+}
+
+int[] array = GreateArray(8);
+PrintArray(array);
