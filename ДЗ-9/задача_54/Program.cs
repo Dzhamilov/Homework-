6504210101ea4fa29fe6b36bc28cae2f7d1ac4d8
Akbar Dzhamilov <akbarshoh-95@mail.ru>
2023-01-27 21:01:36 +0300

    решение задачи 54

---
+++ ДЗ-9/задача_54/Program.cs
@@ -0,0 +1,59 @@
+﻿/*Задайте двумерный массив. Напишите программу, которая 
+упорядочит по убыванию элементы каждой строки двумерного массива.
+Например, задан массив:
+1 4 7 2
+5 9 2 3
+8 4 2 4
+В итоге получается вот такой массив:
+7 4 2 1
+9 5 3 2
+8 4 4 2   */
+
+int[,] GetMatrix(int rows, int colums)
+{
+    int[,] matrix = new int[rows, colums];
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i,j] = new Random().Next(0, 10);
+        }
+    }
+    return matrix; 
+}
+
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write(matrix[i,j] + " ");
+        }
+        Console.WriteLine();
+    }
+}
+
+void SortMatrixRows(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            for (int k = 0; k < matrix.GetLength(1) - j -1; k++)
+            {
+                if (matrix[i,k] < matrix[i,k+1])
+                {
+                    int temp = matrix[i,k];
+                    matrix[i,k] = matrix[i,k+1];
+                    matrix[i,k+1] = temp;
+                }
+            }
+        }
+    } 
+}
+int[,] matrix1 = GetMatrix(3,4);
+PrintMatrix(matrix1);
+Console.WriteLine("Сортированный массив ");
+SortMatrixRows(matrix1);
+PrintMatrix(matrix1);
